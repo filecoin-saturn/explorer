6b4a96232172aba67d90be970c1d55e90bc9870a
Pedro Oliveira <jopedroliveira@users.noreply.github.com>
2023-02-24 13:59:42 +0000

    Add state for Location (#45)
    **Why:**
    
    - Closes #13 
    
    **These changes address the need by:**
    
    - Add filter method by location id
    - Add filter method by country id

--- src/hooks/useLocations.tsx
+++ src/hooks/useLocations.tsx
@@ -12,7 +12,19 @@ type Locations = Location[];
 export const useLocations = () => {
   const [locations, setLocations] = useState<Locations>([]);
 
-  return { locations, setLocations };
+  const getLocationById = (queryLocationId: string) => {
+    return locations.filter((location) => location.id === queryLocationId)
+  }
+
+  const getLocationByCountryId = (queryCountryId: string) => {
+    return locations.filter((location) => location.countryId === queryCountryId)
+  }
+
+  return { 
+    locations,
+    setLocations,
+    getLocationById,
+    getLocationByCountryId };
 };
 
 export default useLocations;
