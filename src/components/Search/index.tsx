fb1fadd3147f51163b05b0fe3e9a735b41b56a31
Pedro Oliveira <poliveira@subvisual.co>
2023-03-06 15:52:52 +0000

    wip: search results

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -7,7 +7,7 @@ import SearchResult from "./SearchResult";
 export const Search = () => {
   const [searchTerm, setSearchTerm] = useState<string | undefined>();
   const [isSearchActive, setIsSearchActive] = useState<boolean>(false);
-
+  
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -55,10 +55,3 @@ export const Search = () => {
 
 export default Search;
 
-/* 
-<div>
-  <h3 className="Search">{children}</h3>
-  <h4 className="Search-result">{children}</h4>
-  <p className="Search-resultLabel">{children}</p>
-</div> 
-*/
