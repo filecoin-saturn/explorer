cb30038d8264929cbf9f32d7701598b9fc4afb0c
Pedro Oliveira <poliveira@subvisual.co>
2023-03-06 15:07:25 +0000

    wip: search results

--- src/components/Search/SearchResult.stories.tsx
+++ src/components/Search/SearchResult.stories.tsx
@@ -1,14 +1,12 @@
-import { SearchResult as SearchResultComponent, SearchResultProps } from "./SearchResult";
-import {Meta, Story} from '@storybook/react'
+import { SearchResult as SearchResultComponent } from "./SearchResult";
 
 export default {
   component: SearchResultComponent,
-} as Meta;
-
-const Template: Story<SearchResultProps> = (args) => <SearchResultComponent {...args} />
-
-export const Icon = Template.bind({});
-Icon.args = {
-  result: {title: 'Portugal', parent: 'Europe'},
-  onClick: () => {}
 };
+
+export const SearchResult = () => (
+  <SearchResultComponent
+    result={{ title: "Portugal", parent: "Europe" }}
+    onClick={() => {}}
+  />
+);
