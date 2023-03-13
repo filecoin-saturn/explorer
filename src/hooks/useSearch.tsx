f68e29d93021d04688dfd83195ff7269f5996f69
Pedro Oliveira <poliveira@subvisual.co>
2023-03-13 18:16:15 +0000

    wip: search bar

---
+++ src/hooks/useSearch.tsx
@@ -0,0 +1,35 @@
+import MiniSearch from "minisearch";
+import useContinents from "./useContinents";
+import useCountries from "./useCountries";
+import useLocations from "./useLocations";
+import useNodes from "./useNodes";
+
+const useSearch = () => {
+  const { continents } = useContinents();
+  // const { countries } = useCountries();
+  const { locations } = useLocations();
+  const { nodes } = useNodes();
+
+  const documents = [...continents] //, ...countries, ...locations, ...nodes]
+  
+  const searchEngine = new MiniSearch({
+    fields: ["name"],
+    storeFields: ["id"],
+  });
+  searchEngine.addAll(documents);
+
+  const search = (searchQuery: string) => {
+    const searchResults = searchEngine.search(searchQuery.toLowerCase(), {
+      prefix: true,
+      fuzzy: 0.2,
+    });
+    
+    const resultsIds = searchResults.map((e) => e.id)
+    const results = documents.filter((doc) => resultsIds.includes(doc.id)) 
+    return results?.slice(0,3);
+  };
+
+  return { search };
+};
+
+export default useSearch;
