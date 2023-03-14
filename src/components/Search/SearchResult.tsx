f049559aef3d0efdc8bf283cf5e604583de9a04c
Pedro Oliveira <poliveira@subvisual.co>
2023-03-14 12:17:20 +0000

    wip: search results click

--- src/components/Search/SearchResult.tsx
+++ src/components/Search/SearchResult.tsx
@@ -1,13 +1,13 @@
 import './SearchResult.css';
 
 export type SearchResultProps = {
-  result: {title:string, parent: string};
-  onClick: () => void;
+  result: {id: string, title:string, parent: string};
+  onClick: (id: string) => void;
 }
 
 export const SearchResult  = ({result, onClick} : SearchResultProps ) => {
   return (
-    <div className='SearchResult' onClick={() => onClick()}>
+    <div className='SearchResult' onClick={() => {console.log("click"); onClick(result.id)}}>
       <div className='SearchResult-name'>{result.title}</div>
       <div className='SearchResult-parent'>{result.parent}</div>
     </div>
