21c6dd4d48a33cc3025e14b5018d9a055d645f42
Pedro Oliveira <poliveira@subvisual.co>
2023-03-14 18:26:13 +0000

    wip: add suggestion parsing

--- src/components/Search/SearchResult.stories.tsx
+++ src/components/Search/SearchResult.stories.tsx
@@ -1,12 +1,19 @@
+import { Country } from "../../hooks/useCountries";
 import { SearchResult as SearchResultComponent } from "./SearchResult";
 
 export default {
   component: SearchResultComponent,
 };
 
-export const SearchResult = () => (
-  <SearchResultComponent
-    result={{ id: 'PT', title: "Portugal", parent: "Europe" }}
-    onClick={() => {}}
-  />
-);
+export const SearchResult = () => {
+  const country = {
+    shape: [],
+    center: [],
+    continentId: ["AF"],
+    id: "ZW",
+    type: 1,
+    idAlpha3: "ZWE",
+    name: "Zimbabwe",
+  } as Country;
+  return <SearchResultComponent result={country} onClick={() => {}} />;
+};
