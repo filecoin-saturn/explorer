35639269c612437152056b8fea70b2b37a810209
Pedro Oliveira <poliveira@subvisual.co>
2023-03-14 17:00:55 +0000

    wip: results parsing

--- src/components/Search/SearchResult.tsx
+++ src/components/Search/SearchResult.tsx
@@ -1,15 +1,22 @@
 import "./SearchResult.css";
+import { Continent } from "../../hooks/useContinents";
+import { Country } from "../../hooks/useCountries";
+import { Location } from "../../hooks/useLocations";
+import { Node } from "../../hooks/useNodes";
 
 export type SearchResultProps = {
-  result: { id: string; title: string; parent: string };
+  result: Continent | Country | Location | Node;
   onClick: (id: string) => void;
 };
 
 export const SearchResult = ({ result, onClick }: SearchResultProps) => {
+  const name = result.hasOwnProperty("name") ? result.name : result.id;
+  const parent = name;
+
   return (
     <div className="SearchResult" onClick={() => onClick(result.id)}>
-      <div className="SearchResult-name">{result.title}</div>
-      <div className="SearchResult-parent">{result.parent}</div>
+      <div className="SearchResult-name">{name}</div>
+      <div className="SearchResult-parent">{parent}</div>
     </div>
   );
 };
