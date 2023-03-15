adce4e1c0c2d490a7c4e1a6d7461dca9393cb803
Pedro Oliveira <poliveira@subvisual.co>
2023-03-15 17:31:44 +0000

    uth: accessibility for toolbar buttons

--- src/components/ToolbarButton/index.tsx
+++ src/components/ToolbarButton/index.tsx
@@ -1,26 +1,32 @@
 import "./index.css";
 import { Icon } from "../Icon";
+import classnames from "classnames";
 
 export type ToolbarButtonProps = {
-  isActive: boolean,
-  onClick: () => void,
-  iconName: string,
-  title: string,
-  subtitle: string
-}
+  isActive: boolean;
+  onClick: () => void;
+  iconName: string;
+  title: string;
+  subtitle: string;
+};
 
-export const ToolbarButton = ( {isActive, onClick, iconName, title, subtitle} : ToolbarButtonProps ) => {
-  let className = `ToolbarButton ${isActive ? "active" : ""}`;
+export const ToolbarButton = ({
+  isActive,
+  onClick,
+  iconName,
+  title,
+  subtitle,
+}: ToolbarButtonProps) => {
+  const className = classnames("ToolbarButton", { active: isActive });
   return (
-    <div className={className}
-      onClick={onClick}>
-      <Icon name={iconName} className={'ToolbarButton-icon'} />
+    <button className={className} onClick={onClick}>
+      <Icon name={iconName} className={"ToolbarButton-icon"} />
       <div className="ToolbarButton-description">
         <div className="ToolbarButton-title">{title}</div>
         <div className="ToolbarButton-subtitle">{subtitle}</div>
       </div>
-    </div>
-  )
-}
+    </button>
+  );
+};
 
-export default ToolbarButton
+export default ToolbarButton;
