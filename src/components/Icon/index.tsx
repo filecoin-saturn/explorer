fb9c4a20789fe9d203379280117f881b9ad456e6
Pedro Oliveira <poliveira@subvisual.co>
2023-03-20 22:44:08 +0000

    HOTFIX: missing assets

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,8 +1,25 @@
+import { url } from "inspector";
 import { ReactSVG } from "react-svg";
 
 export type IconProps = { name: string; className: string };
 
+const checkAssetExists = async (url: string): Promise<boolean> => {
+  try {
+    const response = await fetch(url, { method: "HEAD" });
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+};
+
 export const Icon = ({ name, className }: IconProps) => {
+  let src;
+  src = `${process.env.PUBLIC_URL}/assets/icons/${name}.svg`;
+  if (!checkAssetExists(src)) {
+    console.log(">>>>>");
+    src = `${process.env.PUBLIC_URL}/assets/icons/regions/city.svg`;
+  }
+
   return (
     <ReactSVG
       src={`${process.env.PUBLIC_URL}/assets/icons/${name}.svg`}
