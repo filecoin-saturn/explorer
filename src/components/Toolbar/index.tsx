d59b258169e7b4e18788c5ac0e7a3c0e86a15c1e
João Francisco Reis <joaofranciscoreis13@outlook.com>
2023-03-20 19:01:02 +0000

    fix toolbar icons

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -6,9 +6,9 @@ import ToolbarButton from "../ToolbarButton";
 import AppContext, { ViewMode } from "../../contexts/AppContext";
 
 const toolbarOptions = [
-  { viewMode: ViewMode.Density, iconName: "nodes", title: "Density" },
-  { viewMode: ViewMode.Cluster, iconName: "nodes", title: "Cluster" },
-  { viewMode: ViewMode.Heatmap, iconName: "load", title: "Load" },
+  { viewMode: ViewMode.Density, iconName: "tb-density", title: "Density" },
+  { viewMode: ViewMode.Cluster, iconName: "tb-nodes", title: "Cluster" },
+  { viewMode: ViewMode.Heatmap, iconName: "tb-load", title: "Load" },
 ];
 
 type ViewModeButtonsProps = {
