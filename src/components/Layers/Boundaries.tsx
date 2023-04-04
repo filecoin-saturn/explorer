0fbcf28825f1b742cb352301552df009783d0392
Pedro Oliveira <jopedroliveira@users.noreply.github.com>
2023-04-04 16:24:19 +0100

    Po/map performance tweaks (#119)
    **Why:**
    
    - Closes #120
    
    **These changes address the need by:**
    
    - Removes entity hover update 
    - Removes paint and hover paint highlight
    - Adds boundary highlight on hover

--- src/components/Layers/Boundaries.tsx
+++ src/components/Layers/Boundaries.tsx
@@ -7,82 +7,24 @@ export const Boundaries = ({
   max: number | undefined;
   srcId: string;
 }) => {
-  const steps = [
-    max && max > 200 ? max * 0.1 : 20,
-    max && max > 200 ? max * 0.4 : 50,
-    max && max > 200 ? max * 0.6 : 380,
-  ];
-
   const boundaryLayer = {
     id: "boundaries-fill",
     type: "fill",
     source: srcId,
     "source-layer": "country_boundaries",
     paint: {
-      "fill-color": [
-        "case",
-        ["boolean", ["feature-state", "hover"], false],
-        [
-          "interpolate",
-          ["linear"],
-          ["feature-state", "nodes"],
-          0,
-          "#275cc4",
-          1,
-          "#113CA9",
-          steps[0],
-          "#1E5DFF",
-          steps[1],
-          "#22AFFF",
-          steps[2],
-          "#00FFD1",
-        ],
-        [
-          "interpolate",
-          ["linear"],
-          ["feature-state", "nodes"],
-          0,
-          "transparent",
-          1,
-          "#113CA9",
-          steps[0],
-          "#1E5DFF",
-          steps[1],
-          "#22AFFF",
-          steps[2],
-          "#00FFD1",
-        ],
-      ],
-      "fill-opacity": [
-        "case",
-        ["boolean", ["feature-state", "hover"], false],
-        0.4,
-        0.3,
-      ],
+      "fill-color": "transparent",
       "fill-outline-color": [
         "case",
         ["boolean", ["feature-state", "hover"], false],
-        [
-          "interpolate",
-          ["linear"],
-          ["feature-state", "nodes"],
-          0,
-          "#2A1CF7",
-          1,
-          "#113CA9",
-          steps[0],
-          "#1E5DFF",
-          steps[1],
-          "#22AFFF",
-          steps[2],
-          "#00FFD1",
-        ],
+        "#fff",
         "transparent",
       ],
     },
   };
 
   //@ts-ignore
+  // return <Layer />;
   return <Layer {...boundaryLayer}></Layer>;
 };
 
