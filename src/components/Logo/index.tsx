bc02857e8c449566d8a9abf48cd6040a87a76284
Pedro Oliveira <poliveira@subvisual.co>
2023-04-06 19:25:53 +0100

    wip: logo load anim

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,16 @@
 import "./index.css";
 import Icon from "../Icon";
+import { useEffect, useState } from "react";
 
 export const Logo = () => {
-  return <Icon name="saturn" className={"Logo"} />;
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isReady) return;
+    setTimeout(() => setIsReady(true), 3000);
+  }, []);
+  const classname = isReady ? "Logo" : "Logo-load";
+  return <Icon name="saturn" className={classname} />;
 };
 
 export default Logo;
