254a29662540dce058a64cb1e1d3c3ed3f4b0ab5
Pedro Oliveira <poliveira@subvisual.co>
2023-04-11 11:15:54 +0100

    HOTFIX: logo loading time

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,15 +1,17 @@
 import "./index.css";
 import Icon from "../Icon";
 import { useEffect, useState } from "react";
+import useNodes from "../../hooks/useNodes";
 
 export const Logo = () => {
+  const { nodes } = useNodes();
   const [isReady, setIsReady] = useState<boolean>(false);
 
   useEffect(() => {
-    if (isReady) return;
-    setTimeout(() => setIsReady(true), 3000);
+    if (nodes.length === 0 || isReady) return;
+    setIsReady(true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [nodes]);
   const classname = isReady ? "Logo" : "Logo-load";
   return <Icon name="saturn" className={classname} />;
 };
